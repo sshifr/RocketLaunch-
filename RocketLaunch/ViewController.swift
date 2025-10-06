c281740f3b1581f41d2ba53e53052c52effc9c43
Vlad <v.gorbanev08@gmail.com>
2025-10-06 12:38:58 +0400

    Add animation

--- RocketLaunch/ViewController.swift
+++ RocketLaunch/ViewController.swift
@@ -20,6 +20,15 @@ class ViewController: UIViewController {
     }
 
     @IBAction func powerButtonPressed(_ sender: Any) {
+        rocketContentView.isHidden = false
+        successLabel.isHidden = true
+        let frame = rocketImageView.frame
+        rocketImageView.frame.origin.y = 1000
+        UIView.animate(withDuration: 3.5, animations: {
+            self.rocketImageView.frame = frame
+        }) { (_) in
+            self.successLabel.isHidden = false
+        }
     }
     
 }
