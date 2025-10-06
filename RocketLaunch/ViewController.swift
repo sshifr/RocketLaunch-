891da41cb35c4847aaf71a1fd8f8f3731cd87f8b
Vlad <v.gorbanev08@gmail.com>
2025-10-06 12:19:03 +0400

    Add connections

--- RocketLaunch/ViewController.swift
+++ RocketLaunch/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var rocketContentView: UIView!
+    @IBOutlet weak var rocketImageView: UIImageView!
+    @IBOutlet weak var successLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func powerButtonPressed(_ sender: Any) {
+    }
+    
 }
 
