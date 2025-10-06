a93acfebb75daaf07e7828f2f5e499a3155daaa7
Vlad <v.gorbanev08@gmail.com>
2025-10-06 11:37:46 +0400

    Initial Commit

---
+++ RocketLaunchTests/RocketLaunchTests.swift
@@ -0,0 +1,17 @@
+//
+//  RocketLaunchTests.swift
+//  RocketLaunchTests
+//
+//  Created by Влад on 06.10.2025.
+//
+
+import Testing
+@testable import RocketLaunch
+
+struct RocketLaunchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
